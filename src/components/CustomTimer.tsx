80e538ed08f29f82786461fa0b634c2e56dff0bf
m-dombkowski <dombkowskimateusz@gmail.com>
2025-11-09 13:07:42 +0100

    refractoring CustomTimer component

--- src/components/CustomTimer.tsx
+++ src/components/CustomTimer.tsx
@@ -1,29 +1,38 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { useTimer, useTimerDispatch } from "../stores/TimerStore";
 
 const Input = styled.input<{ $primary?: boolean }>`
   padding: 10px;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
 export default function CustomTimer() {
   const [timeValue, setTimeValue] = useState<number | null>(0);
+  const context = useTimer();
+  const dispatch = useTimerDispatch();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTimeValue(e.target?.value ? parseInt(e.target.value) : null);
+    setTimeValue(e.target.valueAsNumber / 1000); // Convert milliseconds to seconds
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Submitted time value:", timeValue);
+    dispatch({ type: "SET_CURRENT_TIMER", currentTimer: timeValue ?? 0 });
+    console.log(context.currentTimer);
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       <Input
         placeholder="Type time value "
-        type="text"
+        type="time"
         onChange={handleChange}
       />
       <Input placeholder="submit " type="submit" />
-    </form>
+    </Form>
   );
 }
