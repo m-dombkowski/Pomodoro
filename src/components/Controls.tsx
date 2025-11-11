be72606ac383259864206543a56ae56543d2fc09
m-dombkowski <dombkowskimateusz@gmail.com>
2025-11-11 13:09:07 +0100

    func: added start/stop handlers for controls button

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,7 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faPause, faPlay, faRepeat } from "@fortawesome/free-solid-svg-icons";
+import {
+  manageCountDown,
+  useTimer,
+  useTimerDispatch,
+} from "../stores/TimerStore";
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,18 +16,44 @@ const Wrapper = styled.div`
   margin: 50px 0;
 `;
 
-const ControlButton = styled.button`
+const ControlButton = styled.button<{ $disabled?: boolean }>`
   padding: 10px 20px;
   font-size: 16px;
+  ${(props) =>
+    props.$disabled &&
+    css`
+      opacity: 0.5;
+      pointer-events: none;
+    `}
 `;
 
 export default function Controls() {
+  const dispatch = useTimerDispatch();
+  const context = useTimer();
+
+  const handleStart = () => {
+    dispatch({
+      type: "START_TIMER",
+      startTime: context.startTime ?? context.currentTimer,
+    });
+
+    manageCountDown(context.currentTimer, dispatch, true);
+  };
+
+  const handleStop = () => {
+    dispatch({
+      type: "STOP_TIMER",
+      stopTime: context.currentTimer,
+    });
+    manageCountDown(context.currentTimer, dispatch, false);
+  };
+
   return (
     <Wrapper>
-      <ControlButton>
+      <ControlButton $disabled={context.isRunning} onClick={handleStart}>
         <FontAwesomeIcon icon={faPlay} />
       </ControlButton>
-      <ControlButton>
+      <ControlButton onClick={handleStop}>
         <FontAwesomeIcon icon={faPause} />
       </ControlButton>
       <ControlButton>
